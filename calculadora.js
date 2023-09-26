3a245b319daa64eedaec67ecf13fb54ab07d2afe
Danielgv010 <danielgarciav010@gmail.com>
2023-09-26 13:37:51 +0200

    suma

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+function sumar(){
+    let num1 = document.getElementById("num1");
+    let num2 = document.getElementById("num2");
+    let res = num1+num2;
+    document.getElementById("res").value = res;
+}
