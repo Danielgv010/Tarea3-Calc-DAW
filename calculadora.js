644364dd3d4e116b838956522cdbd075c2475c6f
Danielgv010 <danielgarciav010@gmail.com>
2023-09-26 13:39:35 +0200

    resta

--- calculadora.js
+++ calculadora.js
@@ -4,3 +4,9 @@ function sumar(){
     let res = num1+num2;
     document.getElementById("res").value = res;
 }
+function restar(){
+    let num1 = document.getElementById("num1");
+    let num2 = document.getElementById("num2");
+    let res = num1-num2;
+    document.getElementById("res").value = res;
+}
