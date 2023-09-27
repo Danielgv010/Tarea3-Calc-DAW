8cece80a6090142e0aed53c46759bb3b54b2b6e1
Danielgv010 <danielgarciav010@gmail.com>
2023-09-27 12:43:17 +0200

    Edicion suma

--- calculadora.js
+++ calculadora.js
@@ -3,12 +3,13 @@ function sumar(){
     let num2 = document.getElementById("num2");
     let res = num1+num2;
     document.getElementById("res").value = res;
+    //Modifico la funcion de sumar
 }
 function restar(){
     let num1 = document.getElementById("num1");
     let num2 = document.getElementById("num2");
     let res = num1-num2;
-
+}
 function dividir(){
     let num1 = document.getElementById("num1");
     let num2 = document.getElementById("num2");
